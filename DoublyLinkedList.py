e699bf623188e6ae18b8ce8729b2d03230868915
PlumTester <dugald.shannon12@gmail.com>
2020-04-24 10:05:52 +1000

    Node class and shell DoublyLinkedList

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,3 +1,58 @@
-print("test test")
-print("bruh")
-print("stawp")
+
+class Node:
+
+    def __init__(self, element):
+        self.__element = element
+        self.__next = None
+        self.__prev = None
+
+    def __del__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def getElement(self):
+        return self.__element
+
+    def setElement(self, element):
+        self.__element = element
+
+    def getNext(self):
+        return self.__next
+
+    def setNext(self, next):
+        self.__next = next
+
+    def getPrev(self):
+        return self.__prev
+
+    def setPrev(self, prev):
+        self.__prev = prev
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+        
+    def toString(self):
+        pass
+
+    def size(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def first(self):
+        pass
+
+    def last(self):
+        pass
+
+
+
+
